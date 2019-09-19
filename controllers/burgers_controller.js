ce60424276ab7ef9b8e147e49ec908b836aa5a74
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-09-19 22:53:18 +1000

    Setup - config, model, controller(incomplete)

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger");
+
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne()
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne()
+});
